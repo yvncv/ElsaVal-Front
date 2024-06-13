2dfb22026e618c92cf1e5e6e7809a99ef6ae2c15
yvncv <146147151+yvncv@users.noreply.github.com>
2024-06-13 11:26:57 -0500

    Diseño navbar

--- frontend/src/components/BarraBusqueda.tsx
+++ frontend/src/components/BarraBusqueda.tsx
@@ -18,12 +18,18 @@ const BarraBusqueda: React.FC<Props> = ({ loggedInUser }) => {
   };
 
   return (
-    <Navbar expand="lg" bg="light" variant="light" className="mb-3">
+    <Navbar expand="lg" bg="dark" variant="dark" className=""  style={{ color: '#ede8da' }}>
       <Container>
-        <Navbar.Brand as={Link} to="/">Página Web</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/" style={{ padding: 0 }}>
+          <img
+            src="https://scontent.flim28-2.fna.fbcdn.net/v/t39.30808-6/402138489_367665279123466_5898536179762415568_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFIwTA7XydpzdRfpJ9hJV8-U2AAavEGsARTYABq8QawBCci6fT7goT_PUGCcBNqs0swZnl-qKv9RT-Qvq2YVVS_&_nc_ohc=6j0MWbcoWpEQ7kNvgEXx82n&_nc_ht=scontent.flim28-2.fna&oh=00_AYBEL2u2tZAYQgP_3qQs7C7Y-ghmz9UPcZbDox5PLyFjEA&oe=666FB79A"
+            alt="Logo"
+            style={{ width: '60px', height: 'auto', borderRadius: '50%' }}
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto"  style={{ color: '#ede8da' }}>
+          <Nav className="ml-auto">
             {loggedInUser ? (
               <>
                 <Nav.Link as={Link} to="/products">Productos</Nav.Link>
@@ -36,7 +42,7 @@ const BarraBusqueda: React.FC<Props> = ({ loggedInUser }) => {
                 <Nav.Link as={Link} to="/orders/:orderId">Detalles de Orden</Nav.Link>
                 <Nav.Link as={Link} to="/client-orders/:clientId">Órdenes de Cliente</Nav.Link>
                 <Nav.Link as={Link} to="/Acercade">Acerca de</Nav.Link>
-                <Nav.Link as={Link} onClick={handleLogout} to="/">Cerrar Sesión</Nav.Link>
+                <Nav.Link as={Link} onClick={handleLogout} to="/" style={{color: "red"}}>Cerrar Sesión</Nav.Link>
               </>
             ) : (
               <>
