6160df80987095623508d372bcb84c553a7dc9a5
An3cd0ta <just.me.edgar.2004@gmail.com>
2024-06-18 02:54:36 -0500

    Se termino la elaboracion del catalogo de productos, alivianando la cantidad de codigo pero manteniendose responsive, se agrego un off canvas para permitir al usuario filtrar por categoria que productos desea visualizar, dejando la opcion de ver todos por defecto.Finalmente se elimino todo tsx relacionado con administracion, quedando solo los que se estan usando, los cuales son la landing page, sus componentes, la nueva pagina de categorias de producto, el login y su registro y la informacion de la empresa. Se continuara manana con el carrusel.

--- frontend/src/pages/NewCategorias.tsx
+++ frontend/src/pages/NewCategorias.tsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
-import { Form, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Carousel } from 'react-bootstrap';
 import { Product } from '../types/Product';
 import './NewCategorias.css';
-/*import OffCanvasCategoria from '../components/OffCanvasCategoria.tsx';*/
 import Offcanvas from 'react-bootstrap/Offcanvas';
-
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFilter} from '@fortawesome/free-solid-svg-icons';
 
 /*INTERFAZ CATEGORIA */
 interface Categoria {
@@ -18,8 +16,6 @@ interface Categoria {
     name: string;
 }
 /*INTERFAZ CATEGORIA */
-
-
 
 function Categorias() {
   const [show, setShow] = useState(false);
@@ -33,7 +29,6 @@ function Categorias() {
   /*se activa cuando hay un cambio de seleccion */
   const handleSelectChange = (event) => {
     const selectedId=parseInt(event.target.value, 10);
-    setCategoriaSeleccionada(selectedId);
     handleClickVerProductos(selectedId);
   };
   
@@ -42,40 +37,56 @@ function Categorias() {
   const handleClickVerProductos = async (categoriaId: number) => {
       try {
         const response = await axios.get(`https://elsaval.com.pe/api/elsaval/products`);
-        const productosCategoria = response.data.data.filter((producto: Product) => producto.category.id === categoriaId);
+        let productosCategoria=response.data.data;
+        if(categoriaId!==0){
+            productosCategoria = response.data.data.filter((producto: Product) => producto.category.id === categoriaId);
+        }
         setProductos(productosCategoria);
-        setCategoriaSeleccionada(categoriaId);
+        setCategoriaSeleccionada(categoriaId);//puede valer 0
       } catch (error) {
         console.error('Error al obtener productos de la categoría:', error);
       }
   };
-  /*como se va ejecutar una sola vez solo lo hara al recargarse la off,canvas por primera vez*/
-  useEffect(
-      () => {
-          // Obtener las categorías de la API
-          axios.get('https://elsaval.com.pe/api/elsaval/categories')
-          .then(response => {
-              setCategorias(response.data.data);
-          })
-          .catch(error => console.error('Error al obtener categorías:',error));
-      }, []/*esto hace que se ejecute una sola vez */
+  /*como se va ejecutar una sola vez solo lo hara al recargarse la offcanvas por primera vez*/
+  useEffect
+  (
+    ()=>{
+        const obtenerCategorias = async () => {
+            // Obtener las categorías de la API
+            axios.get('https://elsaval.com.pe/api/elsaval/categories')
+            .then(response => {
+                setCategorias(response.data.data);
+            })
+            .catch(error => console.error('Error al obtener categorías:',error));
+        }
+        const mostrarProductos= async() =>{
+            //hacer aparecer todos los productos
+            const response = await axios.get(`https://elsaval.com.pe/api/elsaval/products`);
+            let productosCategoria=response.data.data;
+            setProductos(productosCategoria);
+            setCategoriaSeleccionada(0);
+        }
+        obtenerCategorias();
+        mostrarProductos();
+    }
+    ,[]/*esto hace que se ejecute una sola vez */
   );
   return (
-    <div className="categorias-main-container">
-       {/*Ya no habra una columna para las categorias*/}
-        <div className="categorias-column">
-            <h1 className='categorias-title'>Buscar por</h1>
-            <div className="categorias-container">
-                <Button variant="primary" onClick={handleShow}>
+    <div className="Nuestros-productos-main-container">
+        <div className="cabezera-productos">
+            <h1 className='productos-title'>Nuestros productos</h1>
+            <div className="contenedor-btnFiltrar">
+                <Button className='btnFiltrar' onClick={handleShow}>
+                    <FontAwesomeIcon className="FiltrarIcon" icon={faFilter}/>
                     Filtrar
                 </Button>
                 {/*Este es el OFF CANVAS */}
-                <Offcanvas show={show} onHide={handleClose} placement='end' scroll={false} backdrop={true}>
-                    <Offcanvas.Header closeButton>
-                        <Offcanvas.Title>Filtrar por:</Offcanvas.Title>
+                <Offcanvas className="canvas" show={show} onHide={handleClose} placement='end' scroll={false} backdrop={true}>
+                    <Offcanvas.Header className='canvas-header'>
+                        <Offcanvas.Title className="canvas-title">Filtrar por:</Offcanvas.Title>
                     </Offcanvas.Header>
-                    <Offcanvas.Body>
-                        Seleccione una categoria para hacer el filtrado
+                    <Offcanvas.Body className="canvas-body">
+                        Seleccione una categoria
                         
                         {/*ahora se usa lo obtenido de la API en el useeffect para generar las categorias*/}
                         <select
@@ -84,9 +95,10 @@ function Categorias() {
                             onChange={handleSelectChange}
                         >
                             <option value="" disabled>Selecciona una categoría</option>
+                            <option className="categorias-option" key={0} value={0}>Todo</option>
                             {
                                 categorias.map((categoria) => (
-                                        <option key={categoria.id} value={categoria.id}>
+                                        <option className="categorias-option" key={categoria.id} value={categoria.id}>
                                             {categoria.name}
                                         </option>
                                     )
@@ -99,38 +111,44 @@ function Categorias() {
             </div>
         </div>
       
-        {/*sucede cuando la categoria queda seleccionada, fuentez de ortiz*/}
-        {categoriaSeleccionada !== null && (
-            <div className="categorias-productos-column">
-            <h1 className='resultados-title'>¡Nuestr@s {categorias.find(categoria => categoria.id === categoriaSeleccionada)?.name}!</h1>
-            <div className="categorias-productos-row">
-                {productos.map(producto => (
-                <div key={producto.id} className="col-xxl-4">
-                    <Card className="categorias-productos-card">
-                    <Carousel className="categorias-productos-carousel" interval={1000} fade={true}>
-                        {producto.images.map((image, index) => (
-                        <Carousel.Item className="categorias-productos-carousel-item" key={index}>
-                            <img
-                            className="d-block w-100 h-100"
-                            src={image} // Usar image directamente
-                            alt={`Slide ${index + 1}`}
-                            />
-                        </Carousel.Item>
+        {/*sucede cuando la categoria queda seleccionada*/}
+        {
+
+            categoriaSeleccionada !== null &&(
+                <div className="contenedor-resultados">
+                    {categoriaSeleccionada!==0&&(<h1 className='resultados-title'>Resultados de: {categorias.find(categoria => categoria.id === categoriaSeleccionada)?.name}</h1>)}
+                    {categoriaSeleccionada==0&&(
+                        <h1 className='resultados-title'>Resultados de: Todo</h1>
+                    )}
+                    <div className="contenedor-cards">
+                        {productos.map(producto => (
+                        <div key={producto.id} className="col-xxl-4">
+                            <Card className="contenedor-single-card">
+                            <Carousel className="carousel-card" interval={1000} fade={true}>
+                                {producto.images.map((image, index) => (
+                                <Carousel.Item className="contenedor-img-btn-card" key={index}>
+                                    <img
+                                    className="d-block w-100 h-100"
+                                    src={image} // Usar image directamente
+                                    alt={`Slide ${index + 1}`}
+                                    />
+                                </Carousel.Item>
+                                ))}
+                            </Carousel>
+                            <Card.Body className='body-card'>
+                                <Card.Title>{producto.name}</Card.Title>
+                                <Card.Subtitle>{producto.description}</Card.Subtitle>
+                                <Card.Text>Precio: S./{producto.price}</Card.Text>
+                                <Button variant="primary" className='btn_ver_Detalles'>Ver detalles</Button>
+                            </Card.Body>
+                            </Card>
+                        </div>
                         ))}
-                    </Carousel>
-                    <Card.Body>
-                        <Card.Title>{producto.name}</Card.Title>
-                        <Card.Subtitle>{producto.description}</Card.Subtitle>
-                        <Card.Text>Precio: S./{producto.price}</Card.Text>
-                        <Button variant="primary" className='btn_ver_Detalles'>Ver detalles</Button>
-                    </Card.Body>
-                    </Card>
+                    </div>
                 </div>
-                ))}
-            </div>
-            </div>
-        )}
-      {/*fin de categoria seleccionada */}
+            )
+        }
+        {/*fin de categoria seleccionada */}
     </div>
   );
 }
