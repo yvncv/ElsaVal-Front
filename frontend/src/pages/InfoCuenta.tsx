d704f0ea3d7fc4fcc1f33f8ffbd5eaf23d2f133c
An3cd0ta <just.me.edgar.2004@gmail.com>
2024-06-25 23:47:04 -0500

    se avanzo la informacion de cuenta con el manejo de eventos para actualizacion de tel y mail

--- frontend/src/pages/InfoCuenta.tsx
+++ frontend/src/pages/InfoCuenta.tsx
@@ -3,9 +3,15 @@ import { AuthContext } from '../context/AuthContext';
 import { Client } from '../types/Client';
 import {Form,Button} from 'react-bootstrap';
 import './InfoCuenta.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPenToSquare} from '@fortawesome/free-solid-svg-icons';
+
+
+
 const Detalles: React.FC = () => {
   const { loggedInUser } = useContext(AuthContext);
   const [clientData, setClientData] = useState<Client | null>(null);
+  const [disabled,setDisabled]=useState(true);
 
   useEffect(() => {
     if (loggedInUser) {
@@ -26,30 +32,46 @@ const Detalles: React.FC = () => {
   if (!clientData) {
     return <p>Cargando...</p>;
   }
-
+  //variables
   const { user,contact_number,street_address } = clientData;
   const userName = user.name;
   const userEmail = user.email;
-
   return (
     <>
       <Form className='FormInfo'>
         <h1>Información de la Cuenta</h1>
         <Form.Group className='GroupForm'>
-          <Form.Label>Nombre del Usuario: {userName}</Form.Label>
+          <Form.Label className='lblForm'>Nombre del Usuario: {userName}</Form.Label>
         </Form.Group>
         <Form.Group className='GroupForm'>
-          <Form.Label>Correo Electrónico: {userEmail}</Form.Label>
+          <Form.Label className='lblForm'>Correo Electrónico: {userEmail}</Form.Label>
         </Form.Group>
         <Form.Group className='GroupForm'>
           <Form.Group className='SubGroup'>
-            <Form.Label>Número de teléfono:</Form.Label>
-            <Button className="btnEditar" variant="primary">Editar</Button>
+            <Form.Label className='lblForm'>Número de teléfono:</Form.Label>
+            <Button className="btnEditar" variant="primary" title="Editar" onClick={()=>{setDisabled(!disabled)}}>
+              <FontAwesomeIcon icon={faPenToSquare}/>
+            </Button>
           </Form.Group>
           {/*configuracion de telefono internacional*/}
-          <Form.Control type="text">{contact_number}</Form.Control>
-          <Form.Label>Dirección de envío:</Form.Label>
-          <Form.Control className="txtDireccion" type="text" as="textarea">{street_address}</Form.Control>
+          <Form.Control 
+          className="txtTelefono"
+          type="text"
+          placeholder='Ejm: 994256741'
+          disabled={disabled}
+          
+          >
+            {contact_number}
+          </Form.Control>
+          <Form.Label className='lblForm'>Dirección de envío:</Form.Label>
+          <Form.Control 
+          className="txtDireccion" 
+          type="text" 
+          as="textarea" 
+          placeholder='Ejm: 123 Avenida Primavera, Ciudad Jardín, Estado del Sol, País Imaginario'
+          disabled={disabled}>
+            {street_address}
+          </Form.Control>
         </Form.Group>
       </Form>
     </>
