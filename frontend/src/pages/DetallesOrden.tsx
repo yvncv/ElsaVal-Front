7a515ef5198425916f3a7ee447c0262702c8f34e
An3cd0ta <just.me.edgar.2004@gmail.com>
2024-06-26 23:45:24 -0500

    avance del 75% del historial de Ordenes

--- frontend/src/pages/DetallesOrden.tsx
+++ frontend/src/pages/DetallesOrden.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import OrderDetailsPDF from '../components/OrderDetailsPDF.tsx';
 const DetallesOrden = ()=>{
+    // Función para formatear el costo de envío
+    const formatDeliveryPrice = (deliveryPrice: string | null): string => {
+        if (!deliveryPrice || isNaN(parseFloat(deliveryPrice))) {
+                return 'S/. 00.00';
+        } 
+        else {
+                return `S/. ${parseFloat(deliveryPrice).toFixed(2)}`;
+        }
+    };
+    
+        // Función para formatear el subtotal y el costo total
+    const formatCurrency = (amount: string): string => {
+        const numericAmount = parseFloat(amount);
+        if (isNaN(numericAmount)) {
+            return 'S/. 00.00';
+        } 
+        else {
+            return `S/. ${numericAmount.toFixed(2)}`;
+        }
+    };
+    
     return(
         <div></div>
     );
--- frontend/src/pages/HistorialOrdenes.tsx
+++ frontend/src/pages/HistorialOrdenes.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { Order } from '../types/Order';
 import { AuthContext } from '../context/AuthContext';
-import { PDFDownloadLink } from '@react-pdf/renderer';
-import OrderDetailsPDF from '../components/OrderDetailsPDF.tsx';
+
 import OrdenesCard from '../components/OrdenesCard.tsx';
 import './HistorialOrdenes.css';
 const HistorialOrdenes = () => {
@@ -41,27 +40,6 @@ const HistorialOrdenes = () => {
 
         fetchOrders();
     }, [loggedInUser]);//este metodo se debe quedar aqui entonces
-
-    // Función para formatear el costo de envío
-    const formatDeliveryPrice = (deliveryPrice: string | null): string => {
-        if (!deliveryPrice || isNaN(parseFloat(deliveryPrice))) {
-            return 'S/. 00.00';
-        } else {
-            return `S/. ${parseFloat(deliveryPrice).toFixed(2)}`;
-        }
-    };
-
-    // Función para formatear el subtotal y el costo total
-    const formatCurrency = (amount: string): string => {
-        const numericAmount = parseFloat(amount);
-        if (isNaN(numericAmount)) {
-            return 'S/. 00.00';
-        } else {
-            return `S/. ${numericAmount.toFixed(2)}`;
-        }
-    };
-
-
 
     return (
         <div className='OrderContainer'>
