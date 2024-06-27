b6f459d210d246e45ffc86f7a66f4ce50f97b05d
yvncv <146147151+yvncv@users.noreply.github.com>
2024-06-27 17:42:50 -0500

    Detalles de orden

--- frontend/src/components/OrderDetailsPDF.tsx
+++ frontend/src/components/OrderDetailsPDF.tsx
@@ -49,7 +49,20 @@ const styles = StyleSheet.create({
   tableCell: {
     margin: 5,
     fontSize: 10,
-    width: '25%',
+    flex: 1,
+    textAlign: 'left',
+  },
+  tableCellCenter: {
+    margin: 5,
+    fontSize: 10,
+    flex: 1,
+    textAlign: 'center',
+  },
+  tableCellRight: {
+    margin: 5,
+    fontSize: 10,
+    flex: 1,
+    textAlign: 'right',
   },
   subtotalRow: {
     flexDirection: 'row',
@@ -70,50 +83,55 @@ const styles = StyleSheet.create({
   },
 });
 
-const OrderDetailsPDF = ({ order }: { order: Order }) => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <View style={styles.titleContainer}>
-          <Image src={"/images/ElsaVal_Logo.png"} style={styles.logoImage} />
-          <Text style={styles.title}>RUC: 12345678901</Text>
-        </View>
-        <Text style={styles.text}>
-          <Text style={{ fontWeight: 'bold' }}>Nombres y Apellidos:</Text> {order.client.user.name}
-        </Text>
-        <Text style={styles.text}>
-          <Text style={{ fontWeight: 'bold' }}>Correo Electrónico:</Text> {order.client.user.email}
-        </Text>
-        <Text style={styles.subtitle}>Productos:</Text>
-        <View style={styles.tableHeader}>
-          <Text style={[styles.tableCell, { fontWeight: 'bold' }]}>Nombre Producto</Text>
-          <Text style={[styles.tableCell, { fontWeight: 'bold' }]}>Cantidad</Text>
-          <Text style={[styles.tableCell, { fontWeight: 'bold' }]}>Precio Unitario</Text>
-          <Text style={[styles.tableCell, { fontWeight: 'bold' }]}>Precio Total</Text>
-        </View>
-        {order.products.map((product, index) => (
-          <View key={product.id} style={styles.tableRow}>
-            <Text style={styles.tableCell}>{product.product.name}</Text>
-            <Text style={[styles.tableCell, styles.alignCenter]}>{product.quantity}</Text>
-            <Text style={[styles.tableCell, styles.alignRight]}>S/ {product.unit_price}</Text>
-            <Text style={[styles.tableCell, styles.alignRight]}>S/ {product.total_price}</Text>
+const OrderDetailsPDF = ({ order }: { order: Order }) => {
+  // Ordenar los productos por nombre
+  const sortedProducts = [...order.products].sort((a, b) => a.product.name.localeCompare(b.product.name));
+
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <View style={styles.titleContainer}>
+            <Image src={"/images/ElsaVal_Logo.png"} style={styles.logoImage} />
+            <Text style={styles.title}>RUC: 12345678901</Text>
           </View>
-        ))}
-        <View style={styles.tableRow}>
-          <Text style={[styles.tableCell, { fontWeight: 'bold' }]}>Subtotal:</Text>
-          <Text style={[styles.tableCell, styles.alignRight]}>S/ {order.subtotal_price}</Text>
+          <Text style={styles.text}>
+            <Text style={{ fontWeight: 'bold' }}>Nombres y Apellidos:</Text> {order.client.user.name}
+          </Text>
+          <Text style={styles.text}>
+            <Text style={{ fontWeight: 'bold' }}>Correo Electrónico:</Text> {order.client.user.email}
+          </Text>
+          <Text style={styles.subtitle}>Productos:</Text>
+          <View style={styles.tableHeader}>
+            <Text style={styles.tableCell}>Nombre Producto</Text>
+            <Text style={styles.tableCellCenter}>Cantidad</Text>
+            <Text style={styles.tableCellRight}>Precio Unitario</Text>
+            <Text style={styles.tableCellRight}>Precio Total</Text>
+          </View>
+          {sortedProducts.map((product, index) => (
+            <View key={product.id} style={styles.tableRow}>
+              <Text style={styles.tableCell}>{product.product.name}</Text>
+              <Text style={styles.tableCellCenter}>{product.quantity}</Text>
+              <Text style={styles.tableCellRight}>S/ {product.unit_price}</Text>
+              <Text style={styles.tableCellRight}>S/ {product.total_price}</Text>
+            </View>
+          ))}
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCell}>Subtotal:</Text>
+            <Text style={styles.tableCellRight}>S/ {order.subtotal_price}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCell}>Costo del Envío:</Text>
+            <Text style={styles.tableCellRight}>S/ {order.delivery_price ? order.delivery_price : '0.00'}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCell}>Costo Total del Pedido:</Text>
+            <Text style={styles.tableCellRight}>S/ {order.total_price}</Text>
+          </View>
         </View>
-        <View style={styles.tableRow}>
-          <Text style={[styles.tableCell, { borderLeftWidth: 0, fontWeight: 'bold' }]}>Costo del Envío:</Text>
-          <Text style={[styles.tableCell, styles.alignRight]}>S/ {order.delivery_price || 0}</Text>
-        </View>
-        <View style={styles.tableRow}>
-          <Text style={[styles.tableCell, { fontWeight: 'bold' }]}>Costo Total del Pedido:</Text>
-          <Text style={[styles.tableCell, styles.alignRight]}>S/ {order.total_price}</Text>
-        </View>
-      </View>
-    </Page>
-  </Document>
-);
+      </Page>
+    </Document>
+  );
+};
 
 export default OrderDetailsPDF;
--- frontend/src/pages/DetallesOrden.tsx
+++ frontend/src/pages/DetallesOrden.tsx
@@ -7,38 +7,52 @@ import OrderDetailsPDF from '../components/OrderDetailsPDF.tsx';
 const DetallesOrden = () => {
     const location = useLocation();
     const { order } = location.state;
-    
+
+    // Ordenar los productos por nombre
+    const sortedProducts = [...order.products].sort((a, b) => a.product.name.localeCompare(b.product.name));
+
     return (
-        <div>
-            <h1>Detalles de la Orden N°: {order.id}</h1>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Producto</th>
-                        <th>Cantidad</th>
-                        <th>Precio Unitario</th>
-                        <th>Precio Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {order.products.map((product, index) => (
-                        <tr key={index}>
-                            <td>{product.product.name}</td>
-                            <td>{product.quantity}</td>
-                            <td>{product.unit_price}</td>
-                            <td>{product.total_price}</td>
+        <div style={{width: '20cm', margin: '50px auto'}}>
+            <div className="order-details-container">
+                <div className="order-details-header">
+                    <h1>Detalles de la Orden N°: {order.id}</h1>
+                    <div>
+                        <p>RUC: 12345678901</p>
+                        <p>Nombres y Apellidos: {order.client.user.name}</p>
+                        <p>Correo Electrónico: {order.client.user.email}</p>
+                    </div>
+                </div>
+                <Table striped bordered hover className="order-details-table">
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Cantidad</th>
+                            <th>Precio Unitario</th>
+                            <th>Precio Total</th>
                         </tr>
-                    ))}
-                </tbody>
-            </Table>
-            <p>Subtotal: {order.subtotal_price}</p>
-            <p>Precio de Envío: {order.delivery_price==null}</p>
-            <p>Total: {order.total_price}</p>
-            <PDFDownloadLink 
-            document={<OrderDetailsPDF order={order} />} 
-            fileName={`orden${order.id}.pdf`}>
-                {({ blob, url, loading, error }) => (loading ? 'Generando PDF...' : 'Descargar PDF')}
-            </PDFDownloadLink>
+                    </thead>
+                    <tbody>
+                        {sortedProducts.map((product, index) => (
+                            <tr key={index}>
+                                <td>{product.product.name}</td>
+                                <td>{product.quantity}</td>
+                                <td>S/ {product.unit_price}</td>
+                                <td>S/ {product.total_price}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                <div className="order-details-summary">
+                    <p>Subtotal: S/ {order.subtotal_price}</p>
+                    <p>Precio de Envío: S/ {order.delivery_price ? order.delivery_price : '0.00'}</p>
+                    <p>Total: S/ {order.total_price}</p>
+                </div>
+                <PDFDownloadLink style={{backgroundColor: '#87515a', color: 'white', padding: '10px', borderRadius: '15px', textDecoration: 'none'}}
+                    document={<OrderDetailsPDF order={order} />}
+                    fileName={`orden${order.id}.pdf`}>
+                    {({ blob, url, loading, error }) => (loading ? 'Generando PDF...' : 'Descargar PDF')}
+                </PDFDownloadLink>
+            </div>
         </div>
     );
 }
